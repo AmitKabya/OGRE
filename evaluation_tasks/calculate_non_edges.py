2364c23bce11877b32b272ed4a8f5ad27f736819
unknownuser2021 <78346944+unknownuser2021@users.noreply.github.com>
2021-02-03 00:27:29 +0200

    Evaluation tasks for measuring performance

--- evaluation_tasks/calculate_non_edges.py
+++ evaluation_tasks/calculate_non_edges.py
@@ -4,7 +4,7 @@ For your own dataset, you need its name and path to where it can be found (see d
 different way of loading, add it, in the end you need a networkx graph.
 """
 
-from eval_utils import load_data
+from eval_utils import load_graph
 import os
 import itertools as IT
 import csv
@@ -18,30 +18,16 @@ name3 = "Pubmed"
 name4 = "Yelp"
 name5 = "Reddit"
 
-names = [name1, name2, name3, name4, name5]
+names = [name1]
 
 datasets_path = os.path.join("..", "datasets")
 labels_path = os.path.join("..", "labels")
 
 # how many false edges do you want to extract? try as large as possible.
-number = 20000
+number = 15000
 
 for name in names:
-    if name == "Pubmed":
-        G = nx.read_edgelist(os.path.join(datasets_path, name + ".txt"), delimiter=',')
-    else:
-        if name == "Yelp":
-            H = load_data("yelp.npz")
-            mapping = {}
-            nodes = list(H.nodes())
-            with open(os.path.join(labels_path, "class_map.json")) as json_file:
-                data = json.load(json_file)
-            keys = list(data.keys())
-            for i in range(len(keys)):
-                mapping.update({nodes[i]: keys[i]})
-            G = nx.relabel_nodes(H, mapping)
-        else:
-            G = nx.read_edgelist(os.path.join(datasets_path, name + ".txt"))
+    G = load_graph(datasets_path, name, is_weighted=False)
 
     list_in_embd = list(G.nodes())
 
